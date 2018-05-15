5d0b0337829548e25d00e0aced680bf08ca01fdb
cmustard06 <cmustard06@gmail.com>
2018-05-15 18:28:35 +0800

    add config file

--- util/comm.py
+++ util/comm.py
@@ -4,6 +4,7 @@
 import logging
 import os
 
+from util.config import *
 
 
 def printInfo(file,message):
---
+++ util/config.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# __Author__:cmustard
+
+# 日志记录路径
+LOGFILE = "./test.log"
