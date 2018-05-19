5de3863a5115092a0cc3300c0368ef1c34db0bf1
cmustard06 <cmustard06@gmail.com>
2018-05-19 15:09:11 +0800

    add file

---
+++ fingerprint/wordpress.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# __Author__:cmustard
+
+import util.plugins
+
+define = "Wordpress"
+author = "Andrew Horton"
+version = "0.5"
+description = "Wordpress is an opensource blogging system commonly used as a cms"
+website = "http://www.wordpress.org"
+
+# google hack
+dorks = [
+	'"is proudly powered by WordPress"',
+]
+
