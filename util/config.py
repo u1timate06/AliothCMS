0c90d3efd96fbb5100f5408d740397f2cf6500ec
cmustard06 <cmustard06@gmail.com>
2018-05-29 18:33:03 +0800

    update

--- util/config.py
+++ util/config.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # __Author__:cmustard
 
+import re
+
 # 日志记录路径
 LOGFILE = "./test.log"
 
@@ -12,3 +14,7 @@ DATABASE_USER = "admin"
 DATABASE_PASSD = "password"
 DATABASE = "cms"
 DATABASE_PORT = 27017
+
+
+PLUGIN_CLASSNAME_REGEX = r"class\s+(.*?)\(PluginBase\)"
+CLASSNAMEREGX = re.compile(PLUGIN_CLASSNAME_REGEX)
