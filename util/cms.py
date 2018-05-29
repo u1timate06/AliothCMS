93b16a5d165d5f63eb58eaf2201d3b34eb753d39
cmustard06 <cmustard06@gmail.com>
2018-05-29 18:32:33 +0800

    update

--- util/cms.py
+++ util/cms.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # __Author__:cmustard
 
-import importlib
+import importlib.util
 import re
 import os
+import sys
 
 from multiprocessing import Pool
 from util.comm import getLogger,printInfo
+from util.config import CLASSNAMEREGX,PLUGIN_CLASSNAME_REGEX
 
-PLUGIN_CLASSNAME_REGEX = "class\s+(.*?)\(PluginBase\)"
+
 LOGGER = getLogger()
 PLUGIN_ROOT = "../fingerprint/"
 
@@ -21,29 +23,43 @@ class CmsDector():
 		self.cmsRes = None
 	
 	# 获取插件
-	def get_cmsRes(self, plugin_path):
+
+	def _getCmsRes(self,path):
+		"""
+		
+		:param path:
+		:return:
+		"""
 		try:
-			with open(plugin_path, "r") as f:
-				content = f.read()
-			# 获取文件中的类名
-			class_name = re.findall(PLUGIN_CLASSNAME_REGEX, content)[0]
-			# 获取插件名称
-			filename_ex = os.path.split(plugin_path)[1]
-			# 去掉文件后缀名
-			filename = filename_ex.split(".", -1)[0]
-			# 格式化导入语句
-			importString = "fingerprint.{}".format(filename)
-			# 相对导入
-			ip_module = importlib.import_module(importString)
-			ip_module_cls = getattr(ip_module, class_name)
-			# 获取类
-			plugin_class = ip_module_cls()
-			self.cmsRes = plugin_class.start(self.url)
-			# self.cmsRes.put(self.cmsVul)
+			class_name = self.getClassName(CLASSNAMEREGX,self.getClassString(path))
+			# 判断
+			spec = importlib.util.spec_from_file_location(class_name,path)
+			if spec is None:
+				raise Exception("import module error!!!")
+			moudle = importlib.util.module_from_spec(spec)
+			spec.loader.exec_module(moudle)
+			sys.modules[class_name] = moudle
+			obj = eval("moudle.{}".format(class_name))()
+			self.cmsRes = obj.start(self.url)
 			print(self.cmsRes)
 		except Exception as e:
-			LOGGER.error(printInfo(__file__,"{} function error:{}".format("get_cmsRes",str(e))))
+			LOGGER.error(printInfo(__file__,"function _getCmsRes error.{}".format(str(e))))
 	
+	def getClassString(self,path):
+		if os.path.exists(path):
+			with open(path,'r') as f:
+				content = f.read()
+		else:
+			return ""
+		return content
+		
+	def getClassName(self,pattern,content):
+		result = re.findall(CLASSNAMEREGX,content)
+		if len(result)>0:
+			return result[0]
+		else:
+			return None
+			
 	def run(self, url, thread=10, cmd=False):
 		"""
 		api调用
@@ -70,7 +86,7 @@ class CmsDector():
 		
 		for plugin_path in plugins_list:
 			# self.get_cmsRes(plugin_path)
-			pools.apply_async(func=self.get_cmsRes, args=(plugin_path,))
+			pools.apply_async(func=self._getCmsRes, args=(plugin_path,))
 		
 		pools.close()
 		pools.join()
@@ -79,7 +95,9 @@ class CmsDector():
 
 if __name__ == '__main__':
 	c = CmsDector()
+	path = r"C:\Users\glamor\Desktop\red-club\玉衡CMS指纹识别\AliothCMS\fingerprint\jboss.py"
 	url = "http://182.87.0.20:8081/portal/bsdt.seam"
-	# c.run(url= "http://182.87.0.20:8081/portal/bsdt.seam")
-	c.run(url,thread=10)
+	c.run(url= "http://182.87.0.20:8081/portal/bsdt.seam")
+	# c._getCmsRes(path)
+
 	# get_plugin("123")
